9fa0cb6a8471d52f63c8af1a6fde00080401b36f
david-assouline <68440427+david-assouline@users.noreply.github.com>
2020-10-15 17:11:04 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,80 +1,280 @@
+import pygame
+import neat
+import time
+import os
+import random
 
-[NEAT]
-fitness_criterion     = max
-fitness_threshold     = 100
-pop_size              = 100
-reset_on_extinction   = False
+pygame.font.init()
+WIN_WIDTH = 500
+WIN_HEIGHT = 800
 
-[DefaultGenome]
-# node activation options
-activation_default      = tanh
-activation_mutate_rate  = 0.0
-activation_options      = tanh
+WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+pygame.display.set_caption("Flappy Bird")
 
-# node aggregation options
-aggregation_default     = sum
-aggregation_mutate_rate = 0.0
-aggregation_options     = sum
+SPRITE_BIRDS = [pygame.transform.scale2x(pygame.image.load(r"C:\Users\david\Desktop\Python\Flappy Bird Sprites\imgs\bird1.png")),
+                pygame.transform.scale2x(pygame.image.load(r"C:\Users\david\Desktop\Python\Flappy Bird Sprites\imgs\bird2.png")),
+                pygame.transform.scale2x(pygame.image.load(r"C:\Users\david\Desktop\Python\Flappy Bird Sprites\imgs\bird3.png"))]
 
-# node bias options
-bias_init_mean          = 0.0
-bias_init_stdev         = 1.0
-bias_max_value          = 30.0
-bias_min_value          = -30.0
-bias_mutate_power       = 0.5
-bias_mutate_rate        = 0.7
-bias_replace_rate       = 0.1
+SPRITE_PIPE = pygame.transform.scale2x(pygame.image.load(r"C:\Users\david\Desktop\Python\Flappy Bird Sprites\imgs\pipe.png"))
+SPRITE_BASE = pygame.transform.scale2x(pygame.image.load(r"C:\Users\david\Desktop\Python\Flappy Bird Sprites\imgs\base.png"))
+SPRITE_BG = pygame.transform.scale2x(pygame.image.load(r"C:\Users\david\Desktop\Python\Flappy Bird Sprites\imgs\bg.png"))
 
-# genome compatibility options
-compatibility_disjoint_coefficient = 1.0
-compatibility_weight_coefficient   = 0.5
+FONT_STATS = pygame.font.SysFont("comicsans", 50)
 
-# connection add/remove rates
-conn_add_prob           = 0.5
-conn_delete_prob        = 0.5
 
-# connection enable options
-enabled_default         = True
-enabled_mutate_rate     = 0.01
+class Bird:
+    SPRITES = SPRITE_BIRDS
+    MAX_ROTATION = 25
+    ROT_VEL = 20
+    ANIMATION_TIME = 5
 
-feed_forward            = True
-initial_connection      = full
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.vel = 0
+        self.height = self.y
+        self.img_count = 0
+        self.img = self.SPRITES[0]
 
-# node add/remove rates
-node_add_prob           = 0.2
-node_delete_prob        = 0.2
+    def jump(self):
+        self.vel = -10.5
+        self.tick_count = 0
+        self.height = self.y
 
-# network parameters
-num_hidden              = 0
-num_inputs              = 3
-num_outputs             = 1
+    def move(self):
+        self.tick_count += 1
 
-# node response options
-response_init_mean      = 1.0
-response_init_stdev     = 0.0
-response_max_value      = 30.0
-response_min_value      = -30.0
-response_mutate_power   = 0.0
-response_mutate_rate    = 0.0
-response_replace_rate   = 0.0
+        d = self.vel*self.tick_count + 1.5 * self.tick_count ** 2 #TODO: UNDERSTAND
+        if d >= 16:
+            d = 16
 
-# connection weight options
-weight_init_mean        = 0.0
-weight_init_stdev       = 1.0
-weight_max_value        = 30
-weight_min_value        = -30
-weight_mutate_power     = 0.5
-weight_mutate_rate      = 0.8
-weight_replace_rate     = 0.1
+        if d < 0:
+            d -= 2
 
-[DefaultSpeciesSet]
-compatibility_threshold = 3.0
+        self.y = self.y + d
 
-[DefaultStagnation]
-species_fitness_func = max
-max_stagnation       = 20
-species_elitism      = 2
+        if d < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+        else:
+            if self.tilt > -90:
+                self.tilt -= self.ROT_VEL
 
-[DefaultReproduction]
-elitism            = 2
-survival_threshold = 0.2
+    def draw(self, win):
+        self.img_count += 1
+
+        if self.img_count < self.ANIMATION_TIME:
+            self.img = self.SPRITES[0]
+        elif self.img_count < self.ANIMATION_TIME*2:
+            self.img = self.SPRITES[1]
+        elif self.img_count < self.ANIMATION_TIME*3:
+            self.img = self.SPRITES[2]
+        elif self.img_count < self.ANIMATION_TIME*4:
+            self.img = self.SPRITES[1]
+        elif self.img_count == self.ANIMATION_TIME*4 + 1:
+            self.img = self.SPRITES[0]
+            self.img_count = 0
+
+        if self.tilt <= -80:
+            self.img = self.SPRITES[1]
+            self.img_count = self.ANIMATION_TIME*2
+
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft =  (self.x, self.y)).center) #TODO: UNDERSTAND
+        win.blit(rotated_image, new_rect.topleft)
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
+
+class Pipe:
+    GAP = 200
+    VEL = 5
+
+    def __init__ (self, x):
+        self.x = x
+        self.height = 0
+        self.top = 0
+        self.bottom = 0
+        self.PIPE_TOP = pygame.transform.flip(SPRITE_PIPE, False, True)
+        self.PIPE_BOTTOM = SPRITE_PIPE
+        self.passed = False
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+
+    def move(self):
+        self.x -= self.VEL
+
+    def draw(self, win):
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+    def collide(self, bird):
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        t_point = bird_mask.overlap(top_mask, top_offset)
+
+        if t_point or b_point:
+            return True
+
+        return False
+
+class Base:
+    VEL = 5
+    WIDTH = SPRITE_BASE.get_width()
+    SPRITE = SPRITE_BASE
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.SPRITE, (self.x1, self.y))
+        win.blit(self.SPRITE, (self.x2, self.y))
+
+def blitRotateCenter(surf, image, topleft, angle):
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center = image.get_rect(topleft = topleft).center)
+    surf.blit(rotated_image, new_rect.topleft)
+
+
+def draw_window(win, birds, pipes, base, score):
+
+    win.blit(SPRITE_BG, (0,0))
+
+    for pipe in pipes:
+        pipe.draw(win)
+
+    text = FONT_STATS.render("Score: " + str(score), 1, (255,255,255))
+    win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
+
+    base.draw(win)
+    for bird in birds:
+        bird.draw(win)
+
+    pygame.display.update()
+
+def main(genomes, config):
+    nets = []
+    ge = []
+    birds = []
+
+    for _, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        birds.append(Bird(230, 350))
+        g.fitness = 0
+        ge.append(g)
+
+    bird1 = Bird(230,350)
+    base = Base(730)
+    pipes = [Pipe(700)]
+    score = 0
+    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                quit()
+
+        pipe_ind = 0
+        if len(birds) > 0:
+            if len(pipes) > 1 and birds[0].x > pipes[0].x * pipes[0].PIPE_TOP.get_width():
+                pipe_ind = 1
+        else:
+            run = False
+            break
+
+        for x, bird in enumerate(birds):
+            bird.move()
+            ge[x].fitness += 0.1
+
+            output = nets[x].activate((bird.y, abs(bird.y - pipes[pipe_ind].height), abs(bird.y -
+                                                                                         pipes[pipe_ind].bottom)))
+            if output[0] > 0.5:
+                bird.jump()
+
+        remove = []
+        add_pipe = False
+        for pipe in pipes:
+            for x, bird in enumerate(birds):
+                if pipe.collide(bird):
+                    ge[x].fitness -= 1
+                    birds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
+
+
+                if not pipe.passed and pipe.x < bird1.x:
+                    pipe.passed = True
+                    add_pipe = True
+
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                remove.append(pipe)
+
+            pipe.move()
+
+        if add_pipe:
+            score += 1
+            for g in ge:
+                g.fitness += 5
+            pipes.append(Pipe(600))
+
+        if len(remove) > 0:
+            for x in remove:
+                pipes.remove(x)
+
+        for x, bird in enumerate(birds):
+            if bird.y + bird.img.get_height() >= 730:
+                birds.pop(x)
+                nets.pop(x)
+                ge.pop(x)
+
+        base.move()
+        draw_window(win,birds,pipes,base,score)
+
+
+def run(config_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet,
+                                neat.DefaultStagnation, config_path)
+
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner = p.run(main,50)
+    print("\nBest Genome = \n {!s}".format(winner))
+
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config-feedforward.txt")
+    run(config_path)
